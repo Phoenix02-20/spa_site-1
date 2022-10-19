1a32078dcc76df411c6a5d3925bf20f0fed1bf1d
kpilsrma1 <34863804+kpilsrma1@users.noreply.github.com>
2022-10-19 21:18:38 +0530

    spa

--- website/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from .views import index, UserRegisterView
-from django.urls import path, include
-
-urlpatterns = [
-    path('', views.index, name="index"),
-    path('contact.html', views.contact, name="contact"),
-    path('register/', UserRegisterView.as_view(), name='register'),
-]
